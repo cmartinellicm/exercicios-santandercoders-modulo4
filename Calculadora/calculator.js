183bcaa80160ee549d97335f09ad0a2809619f33
Cássia Martinelli <56940890+cmartinellicm@users.noreply.github.com>
2021-10-25 16:24:23 -0300

    Calculator: fixed exceptions

--- Calculadora/calculator.js
+++ Calculadora/calculator.js
@@ -2,16 +2,19 @@ let calculator = (() => {
     const sum = (entries) => entries.reduce((acc, curr) => acc + curr);
     const subtract = (entries) => entries.reduce((acc, curr) => acc - curr);
     const multiply = (entries) => entries.reduce((acc, curr) => acc * curr);
-    const divide = (entries) =>
-        entries.reduce((acc, curr) => (curr !== 0 ? acc / curr : console.log('Cannot divide by zero!')));
+    const divide = (entries) => {
+        const division = entries.reduce((acc, curr) => acc / curr);
+        return Math.round(division * 10000) / 10000;
+    };
+
     const operation = {
         '+': sum,
         '-': subtract,
         '*': multiply,
         '/': divide,
     };
+
     let entries = [];
-    let history = new Map();
 
     const enter = (data) => {
         entries = [...entries, data];
@@ -19,44 +22,17 @@ let calculator = (() => {
     };
 
     const equals = (operator) => {
-        if (entries.length < 2) {
-            return 'Please insert at least 2 numbers in accumulator';
-        } else {
+        if (entries.length > 1) {
             const operationLog = entries.join(` ${operator} `);
             const result = operation[operator](entries);
 
-            history.set(operationLog, result);
-            return `${operationLog} = ${result}`;
+            return result === Infinity ? `${operationLog} = Cannot divide by zero` : `${operationLog} = ${result}`;
         }
     };
 
-    const reset = () => {
+    const resetAcc = () => {
         entries = [];
     };
 
-    return { enter, equals, reset };
+    return { enter, equals, resetAcc };
 })();
-
-const entry = document.querySelector('input#entry');
-const accumulator = document.querySelector('span#acc');
-const operationResult = document.querySelector('span#result');
-
-const enterButton = document.querySelector('button#enter');
-const equalsButton = document.querySelector('button#equals');
-const resetButton = document.querySelector('button#reset');
-
-enterButton.onclick = function () {
-    accumulator.innerText = entry.value === '' ? 'Invalid entry!' : calculator.enter(+entry.value);
-    entry.value = '';
-};
-
-equalsButton.onclick = function () {
-    const operator = document.querySelector('input[type=radio]:checked');
-    operationResult.innerText = operator === null ? 'Please choose an operation' : calculator.equals(operator.value);
-};
-
-resetButton.onclick = function () {
-    calculator.reset();
-    accumulator.innerText = '';
-    operationResult.innerText = '';
-};
---
+++ Calculadora/index.js
@@ -0,0 +1,49 @@
+const enterButton = document.querySelector('button#enter');
+const equalsButton = document.querySelector('button#equals');
+const resetAccButton = document.querySelector('button#resetAcc');
+const resetHistoryButton = document.querySelector('button#resetHistory');
+
+const accumulator = document.querySelector('span#acc');
+const operationResult = document.querySelector('span#result');
+const historyList = document.querySelector('ul#historyList');
+
+enterButton.onclick = function () {
+    const entry = document.querySelector('input#entry');
+
+    accumulator.innerText = entry.value === '' ? 'Invalid entry!' : calculator.enter(+entry.value);
+    entry.value = '';
+};
+
+equalsButton.onclick = function () {
+    const operator = document.querySelector('input[type=radio]:checked');
+
+    if (operator === null) {
+        operationResult.innerText = 'Please choose an operation';
+    } else {
+        const currentOperation = calculator.equals(operator.value);
+
+        if (currentOperation) {
+            operationResult.innerText = currentOperation;
+            updateHistory(currentOperation);
+        } else {
+            operationResult.innerText = 'Please insert at least 2 numbers in accumulator';
+        }
+    }
+};
+
+resetAccButton.onclick = function () {
+    calculator.resetAcc();
+    accumulator.innerText = '';
+    operationResult.innerText = '';
+};
+
+resetHistoryButton.onclick = function () {
+    historyList.innerText = '';
+};
+
+function updateHistory(currentOperation) {
+    const li = document.createElement('li');
+
+    li.appendChild(document.createTextNode(`${currentOperation}`));
+    historyList.appendChild(li);
+}
